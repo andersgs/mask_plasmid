02875117a9946eee5275fd7868f6b4ce0d97b5f5
Anders Goncalves da Silva <andersgs@gmail.com>
2018-11-04 19:55:58 -0800

    Added control of version number to PBR

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ Some basic tasks related to deployment of mask_plasmids
 '''
 
 import logging
+import os
 
 from invoke import task
 
@@ -29,6 +30,8 @@ def deploy(c, new_version, update_type="patch", bump_version=True):
     '''
     Deploy a new version
     '''
+    pbr_version = {"PBR_VERSION": f{new_version}}
+    os.environ.update(pbr_version)
     c.run("pipenv run pipenv_to_requirements")
     if bump_version:
         c.run(
